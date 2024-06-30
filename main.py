ecd8d8d6b15b5fedcf503aeffeebea959742a1a5
pamalasomchith <somchithpamala@gmail.com>
2024-06-29 21:44:07 -0700

    commit 1

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
-    Code Your Program here
-    """
+    
+    if (num1 >= num2) and (num1 >= num3):
+        maxnum = num1
+    elif (num2 >= num1) and (num2 >= num3):
+        maxnum = num 2
+    else:
+        maxnum = num3
+    
 
     print(f'The greates number is {maxnum}')
     ########################################
